03e658d38b7aa643326fb80c420c24aff84798d8
Haavaala <haavaala@stud.ntnu.no>
2022-03-24 10:24:55 +0100

    fixed function display

--- main.js
+++ main.js
@@ -32,41 +32,45 @@ window.onclick = (e) => {
 };
 
 
-function Living(name, rooms, booked, bathroom, tv){
+function Living(name, rooms, booked, bathroom, tv, bed){
   this.name = name;
   this.rooms = rooms;
   this.booked = booked;
   this.bathroom = bathroom;
   this.tv = tv;
+  this.bed = bed;
   this.rooma = function(){
     return this.rooms -this.booked;
 
   };
-  
+  delete this.bed;
 }
-  let cabin = new Living('Hytta', 10, 3, 1, true)
   let hotel = new Living('Hotellet', 50, 25, 1, true)
-  let motel = new Living('Motel', 20, 15, 1, false)
+  let cabin = new Living('Hytta', 10, 3, 1, true)
+  let motel = new Living('Motel', 20, 15, '1', '2', '3', false)
 
-  document.getElementById('motelname').innerHTML = motel.name;
-  document.getElementById('motelrooms').innerHTML = motel.rooms;
-  document.getElementById('motelbooked').innerHTML = motel.booked;
-  document.getElementById('motelbath').innerHTML = motel.bathroom;
-  document.getElementById('moteltv').innerHTML = motel.tv;
+  
   
   document.getElementById('hotelname').innerHTML = hotel.name;
   document.getElementById('hotelrooms').innerHTML = hotel.rooms;
   document.getElementById('hotelbooked').innerHTML = hotel.booked;
+  document.getElementById('hotela').innerHTML = hotel.rooma();
   document.getElementById('hotelbath').innerHTML = hotel.bathroom;
   document.getElementById('hoteltv').innerHTML = hotel.tv;
 
   document.getElementById('cabinname').innerHTML = cabin.name;
   document.getElementById('cabinrooms').innerHTML = cabin.rooms;
   document.getElementById('cabinbooked').innerHTML = cabin.booked;
+  document.getElementById('cabina').innerHTML = cabin.rooma();
   document.getElementById('cabinbath').innerHTML = cabin.bathroom;
   document.getElementById('cabintv').innerHTML = cabin.tv;
 
-
+  document.getElementById('motelname').innerHTML = motel.name;
+  document.getElementById('motelrooms').innerHTML = motel.rooms;
+  document.getElementById('motelbooked').innerHTML = motel.booked;
+  document.getElementById('motela').innerHTML = motel.rooma();
+  document.getElementById('motelbath').innerHTML = motel.bathroom;
+  document.getElementById('moteltv').innerHTML = motel.tv;
 
 
 const siteCreation= new Date();
