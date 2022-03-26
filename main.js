19b86b5fdbde39bb3e2726a5eaaf51c9ba996716
Haavaala <haavaala@stud.ntnu.no>
2022-03-26 14:40:25 +0100

    added some more comments

--- main.js
+++ main.js
@@ -15,6 +15,7 @@ btns.forEach((btn) => {
 });
 
 //the close popup button, when this is clicked, the popup modul will disappear//
+//task III b//
 close_btns.forEach((btn) => {
     btn.addEventListener("click", () => {
       const popup = btn.closest(".popup");
@@ -22,7 +23,8 @@ close_btns.forEach((btn) => {
       overlay.classList.remove("active");
     });
   });
-//when clicking outside the popup, it will disapear since it removes 'active'//
+//when clicking outside the popup, it will disapear since it removes 'active'. task II a//
+//task III a//
 window.onclick = (e) => {
     if (e.target == overlay) {
         const popups = document.querySelectorAll('.popup');
@@ -31,7 +33,7 @@ window.onclick = (e) => {
     }
 };
 
-//my objects//
+//my objects task I a, b, c, d, e//
 function Living(name, rooms, booked, bathroom, tv, bed){
   this.name = name;
   this.rooms = rooms;
@@ -52,14 +54,17 @@ function Living(name, rooms, booked, bathroom, tv, bed){
   let motel = new Living(['Motel', ' Moss'], 20, 15, 1, false)
 
   //change the name of the hotel to uppercase//
+ //task II c//
   const newHotelName = hotel.name.toUpperCase();
   hotel.name = newHotelName;
 
   //rounding the number down for the hotel.bathroom with a math object//
+  //task II d//
   const newHotelBath = Math.round(hotel.bathroom);
   hotel.bathroom = newHotelBath;
   
   //getting the javascript values to display in the HTML and the webpage by using 'document' and 'getElementById'//
+  //task II b.//
   document.getElementById('hotelname').innerHTML = hotel.name;
   document.getElementById('hotelrooms').innerHTML = hotel.rooms;
   document.getElementById('hotelbooked').innerHTML = hotel.booked;
@@ -82,5 +87,6 @@ function Living(name, rooms, booked, bathroom, tv, bed){
   document.getElementById('moteltv').innerHTML = motel.tv;
 
 //to show which year it is at the bottom of the page I used 'getFullYear' and 'getElementById' to show it in the HTML//
+//task II e//
 const siteCreation= new Date();
 document.getElementById('fullyear').innerHTML = siteCreation.getFullYear();
